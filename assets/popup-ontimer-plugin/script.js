6ee23260c1d1bb348450addf4e2b50d9aeb06627
mrjromero <52896154+mrjromero@users.noreply.github.com>
2021-07-30 17:09:17 -0400

    create github pages

--- assets/popup-ontimer-plugin/script.js
+++ assets/popup-ontimer-plugin/script.js
@@ -1,2 +1,3 @@
 
-(function(a){a(document).ready(function(){a("html").hasClass("is-builder")||a(".mbr-popup[data-on-timer-delay]").each(function(d,b){var c=a(b).attr("data-on-timer-delay");setTimeout(function(){a(b).modal("show")},1E3*c)})})})(jQuery);
+(()=>{document.addEventListener("DOMContentLoaded",()=>{document.querySelector("html").classList.contains("is-builder")||document.querySelectorAll(".mbr-popup[data-on-timer-delay]").forEach((b)=>{var c=b.getAttribute("data-on-timer-delay"),a;"undefined"!==typeof bootstrap?("undefined"!==typeof bootstrap.Modal.getInstance&&(a=bootstrap.Modal.getInstance(b)),a||(a=new bootstrap.Modal(b),{keyboard:!1}),setTimeout(()=>{a.show()},1E3*c)):"undefined"!==typeof jQuery&&setTimeout(()=>{$(b).modal({keyboard:!1})},
+1E3*c);setTimeout(()=>{a.show()},1E3*c)})})})();
